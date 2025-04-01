673b900e188702bab8a82b7c2ea15553d96643b1
Dmitrii Grechko <prime.optimys@mail.ru>
2025-04-01 12:55:09 +0500

    add pytest-cov

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import psycopg2
 
+
+conn = psycopg2.connect(host="localhost", database="test", user="postgres", password="Neduryvyb032025")
+try:
+    with conn:
+        with conn.cursor() as cur:
+            cur.execute("INSERT INTO post VALUES (%s, %s, %s, %s)", (6, 'Monday', '', 2))
+            cur.execute("SELECT * FROM post")
+            rows = cur.fetchall()
+            for i in rows:
+                print(i)
+finally:
+    conn.close()
